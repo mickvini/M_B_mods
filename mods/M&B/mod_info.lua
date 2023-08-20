d43501fa9ff4d30de0064d6c04557950f1bce290
mickvini <93882097+mickvini@users.noreply.github.com>
2023-08-20 20:36:13 +0300

    Update mod_info.lua

--- mods/M&B/mod_info.lua
+++ mods/M&B/mod_info.lua
@@ -1,8 +1,8 @@
 name = "M&B"
 uid = "62e2j64a-53a1-y6sl-32h5-146as576a18u4"
-version = 0.1
-description = "HZ"
-author = "HZ2"
+version = 0.2
+description = "Mickvini and Bobichka Mod Pack For Supreme"
+author = "Bobichka, Mickvini"
 --icon = ""
 selectable = true
 enabled = true
