81df5d0b2680b74ccde28b58fe2b9fd1e0207db4
mickvini <rom.zhen3@rambler.ru>
2024-04-11 18:29:32 +0300

    Update mod_info.lua

--- mods/M&B/mod_info.lua
+++ mods/M&B/mod_info.lua
@@ -1,6 +1,6 @@
 name = "M&B"
 uid = "62e2j64a-53a1-y6sl-32h5-146as576a18u4"
-version = 0.6
+version = 0.7
 description = "Mickvini and Bobichka Mod Pack For Supreme"
 author = "Bobichka, Mickvini"
 icon = "/mods/M&B/mod_info.png"
