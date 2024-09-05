ccb9e451f965cbe24e8377fbb0f853bd8a678fb5
mickvini <rom.zhen3@rambler.ru>
2024-09-05 15:22:08 +0300

    Nuke assist nerf, uef hive drone removed

---
+++ mods/M&B/units/XEB0114/XEB0114_script.lua
@@ -0,0 +1,16 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEB0104/XEB0104_script.lua
+#**  Author(s):  Dru Staltman
+#**
+#**  Summary  :  UEF Engineering tower
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local TConstructionUnit = import('/lua/terranunits.lua').TConstructionStructureUnit
+
+XEB0114 = Class(TConstructionUnit) {
+
+}
+
+TypeClass = XEB0114
---
+++ mods/M&B/units/XEB0214/XEB0214_script.lua
@@ -0,0 +1,16 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEB0204/XEB0204_script.lua
+#**  Author(s):  Dru Staltman
+#**
+#**  Summary  :  UEF Engineering tower
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local TConstructionUnit = import('/lua/terranunits.lua').TConstructionStructureUnit
+
+XEB0214 = Class(TConstructionUnit) {
+
+}
+
+TypeClass = XEB0214
